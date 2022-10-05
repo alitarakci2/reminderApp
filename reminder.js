f3cda1ad4cd76f1d43e7cc789dbad187dda3a2e2
alitarakci2 <alitarakci2@gmail.com>
2022-10-05 17:14:46 +0300

    Local Storage Module Added.

--- reminder.js
+++ reminder.js
@@ -4,6 +4,7 @@ const gorevListesi = document.querySelector(`.gorev-listesi`);
 
 yeniGorevEkleBtn.addEventListener(`click`,gorevEkle);
 gorevListesi.addEventListener(`click`,gorevSilTamamla)
+document.addEventListener(`DOMContentLoaded`, localStorageOku);
 function gorevSilTamamla(e){
     const tiklanilanEleman = e.target;
     if(tiklanilanEleman.classList.contains(`gorev-btn-tamamlandi`)){
@@ -11,45 +12,121 @@ function gorevSilTamamla(e){
         tiklanilanEleman.parentElement.classList.toggle(`gorev-tamamlandi`);
     }
     if(tiklanilanEleman.classList.contains(`gorev-btn-sil`)){
-        
+
+    if(confirm(`Silmek istediginizden emin misiniz`)){
+
         tiklanilanEleman.parentElement.classList.toggle(`kaybol`);
+        const silinecekGorev = tiklanilanEleman.parentElement.children[0].innerText;
+        localStorageSil(silinecekGorev);
 
         tiklanilanEleman.parentElement.addEventListener(`transitionend`, function(){
             tiklanilanEleman.parentElement.remove();
 
         });
+
+    }
+
+
+    
+        
+  
     }
     
 }
 
+function gorevItemOlustur(gorev){
+    const gorevDiv = document.createElement(`div`);
+gorevDiv.classList.add(`gorev-item`);
 
+const gorevLi = document.createElement(`li`);
+gorevLi.classList.add(`gorev-tanim`);
+gorevLi.innerText = gorev;
+gorevDiv.appendChild(gorevLi);
+
+const gorevTamamBtn = document.createElement(`button`);
+gorevTamamBtn.classList.add(`gorev-btn`);
+gorevTamamBtn.classList.add(`gorev-btn-tamamlandi`);
+gorevTamamBtn.innerHTML = ` <i class="fa-solid fa-check"></i>`;
+gorevDiv.appendChild(gorevTamamBtn);
+
+const gorevSilBtn = document.createElement(`button`);
+gorevSilBtn.classList.add(`gorev-btn`);
+gorevSilBtn.classList.add(`gorev-btn-sil`);
+gorevSilBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
+gorevDiv.appendChild(gorevSilBtn);
+
+
+
+
+gorevListesi.appendChild(gorevDiv);
+}
 
 function gorevEkle(e){
 
     e.preventDefault();
 
-    const gorevDiv = document.createElement(`div`);
-    gorevDiv.classList.add(`gorev-item`);
+    if(yeniGorev.value.length > 0){
+        gorevItemOlustur(yeniGorev.value);
+        localStorageKaydet(yeniGorev.value);
+        yeniGorev.value = ``;
+
+    }else{
+        alert(`Bos gorev tanimi olamaz`);
+    }
+
     
-    const gorevLi = document.createElement(`li`);
-    gorevLi.classList.add(`gorev-tanim`);
-    gorevLi.innerText = yeniGorev.value;
-    gorevDiv.appendChild(gorevLi);
 
-    const gorevTamamBtn = document.createElement(`button`);
-    gorevTamamBtn.classList.add(`gorev-btn`);
-    gorevTamamBtn.classList.add(`gorev-btn-tamamlandi`);
-    gorevTamamBtn.innerHTML = ` <i class="fa-solid fa-check"></i>`;
-    gorevDiv.appendChild(gorevTamamBtn);
+}
 
-    const gorevSilBtn = document.createElement(`button`);
-    gorevSilBtn.classList.add(`gorev-btn`);
-    gorevSilBtn.classList.add(`gorev-btn-sil`);
-    gorevSilBtn.innerHTML = `<i class="fa-solid fa-delete-left"></i>`;
-    gorevDiv.appendChild(gorevSilBtn);
+function localStorageArrayDonustur(){
 
-    yeniGorev.value = ``;
+    let gorevler;
 
-    gorevListesi.appendChild(gorevDiv);
+    if(localStorage.getItem(`gorevler`) === null){
+        gorevler = [];
+
+    }else {
+        gorevler = JSON.parse(localStorage.getItem(`gorevler`));
+    }
+
+    return gorevler;
+
+
+}
+function localStorageKaydet(yeniGorev){
+    let gorevler = localStorageArrayDonustur();
+    gorevler.push(yeniGorev);
+    localStorage.setItem(`gorevler`, JSON.stringify(gorevler));
+}
+
+function localStorageOku(){
+
+    let gorevler = localStorageArrayDonustur();
+
+   
+
+    gorevler.forEach(function(gorev){
+        gorevItemOlustur(gorev);
+
+
+    });
+
+ 
+
+
+
+}
+
+function localStorageSil(gorev){
+    let gorevler = localStorageArrayDonustur();
+
+
+
+    const silinecekElemanIndex = gorevler.indexOf(gorev);
+    console.log(silinecekElemanIndex);
+    gorevler.splice(silinecekElemanIndex,1);
+
+    localStorage.setItem(`gorevler`, JSON.stringify(gorevler));
+    
 
 }
